5de3286a25861a35a236a4821e982eeba4a7c585
NewChoBo <whwjdrb1009@gmail.com>
2024-06-18 16:39:20 +0900

    [Gold V] Title: 토마토, Time: 892 ms, Memory: 124996 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -8,20 +8,22 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     private static int M, N;
+    private static final int[][] move = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static final Queue<int[]> queue = new ArrayDeque<>();
+    private static int[][] map = new int[N][];
 
     public static void main(String[] args) throws IOException {
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         M = input[0];
         N = input[1];
+        map = new int[N][];
 
         // 1: 익은 토마토
         // 0: 안익은 토마토
         // -1: 토마토 없음
-        int[][] map = new int[N][];
         for (int i = 0; i < N; i++) {
             map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-
 
         // 시작할때부터 모든 토마토가 익어있으면 0
         // 토마토가 모두 익을 수 없으면 -1 출력
@@ -29,22 +31,12 @@ public class Main {
         // 1. for문을 통해 이미 익어있는 토마토 queue에 삽입 (여기서 모두 익어있는지도 검증)
         // 2. 반복문을 돌면서, 큐에 들어있던 토마토와 인접한 안익은 토마토들을 새로운 큐에 삽입 (날짜 +1)
         // 3. 새로 입력받아야 하는 큐가 텅 빌때까지 반복
-
-        Queue<int[]> queue = new ArrayDeque<>();
-        int unripeTomato = 0;
         int day = -1;
 
         // 1. 안익은 토마토 수, 현재 익은 토마토 파악
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                switch (map[i][j]) {
-                    case 1:
-                        queue.add(new int[]{i, j});
-                        break;
-                    case 0:
-                        unripeTomato++;
-                        break;
-                }
+                if (map[i][j] == 1) queue.add(new int[]{i, j});
             }
         }
 
@@ -52,33 +44,40 @@ public class Main {
         while (!queue.isEmpty()) {
             day++;
             int loopCnt = queue.size();
-            for(int i=0; i<loopCnt; i++) {
+            for (int i = 0; i < loopCnt; i++) {
                 // 현 위치 구하기
                 int[] currentTomato = queue.poll();
                 //인접 미방문 토마토 방문 처리 및 queue 삽입
-                unripeTomato = getCloseTomato(queue, map, currentTomato, unripeTomato);
+                getCloseTomato(currentTomato);
             }
         }
-        if (unripeTomato > 0) day = -1;
+
+        // 4. 안익은 토마토 확인
+        if (!getNoUnripedTomato()) day = -1;
         bw.write(String.valueOf(day));
         bw.flush();
     }
 
-    private static int getCloseTomato(Queue<int[]> queue, int[][] map, int[] currentPoint, int unripeTomato) {
-        int[][] move = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
+    private static void getCloseTomato(int[] currentPoint) {
         for (int[] point : move) {
             int currentX = currentPoint[0] + point[0];
             int currentY = currentPoint[1] + point[1];
-            if(N-1 < currentX || currentX < 0 || M-1 < currentY || currentY < 0) continue;
+            if (N - 1 < currentX || currentX < 0 || M - 1 < currentY || currentY < 0) continue;
 
             int tomato = map[currentX][currentY];
             if (tomato == 0) {
                 queue.add(new int[]{currentX, currentY});
                 map[currentX][currentY] = 1;
-                unripeTomato--;
             }
         }
-        return unripeTomato;
+    }
+
+    private static boolean getNoUnripedTomato() {
+        for (int[] i : map) {
+            for (int j : i) {
+                if (j == 0) return false;
+            }
+        }
+        return true;
     }
 }
