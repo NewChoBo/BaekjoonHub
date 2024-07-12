24d08c9857e9b40b272ec634e058b72c1207a6b1
NewChoBo <whwjdrb1009@gmail.com>
2024-07-12 13:05:10 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 112 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,26 +19,26 @@ public class Main {
             ladderAndSnake.put(input2[0], input2[1]);
         }
 
-        Stack<Counter> stack = new Stack<>();
-        stack.push(new Counter(0, 1));
-        while (!stack.isEmpty()) {
-            Counter counter = stack.pop();
+        Queue<Counter> queue = new ArrayDeque<>();
+        queue.offer(new Counter(0, 1));
+        while (!queue.isEmpty()) {
+            Counter counter = queue.poll();
             int turn = counter.turn + 1;
             int currentPosition = counter.position;
             for (int i = currentPosition + 1; i <= currentPosition + 6; i++) {
                 if (i > 100) break;  // 100을 초과하는 부분에 대해서는 갱신 필요 없음
 
-                if (ladderAndSnake.get(i) != null) {
-                    int j = ladderAndSnake.get(i);
+                Integer j = ladderAndSnake.get(i);
+                if(j == null) {
+                    if(map[i] == 0 || map[i] > turn) {
+                        map[i] = turn;
+                        queue.offer(new Counter(turn, i));   // 조건을 만족하면 stack에 추가
+                    }
+                } else {
                     if (map[j] == 0 || map[j] > turn) {
                         map[j] = turn;
-                        stack.push(new Counter(turn, j));
+                        queue.offer(new Counter(turn, j));
                     }
-                    continue;
-                }
-                if(map[i] == 0 || map[i] > turn) {
-                    map[i] = turn;
-                    stack.push(new Counter(turn, i));   // 조건을 만족하면 stack에 추가
                 }
             }
         }
