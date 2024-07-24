adabd7d5d47baacd06eece0700d25cb89d6d463e
NewChoBo <whwjdrb1009@gmail.com>
2024-07-24 14:04:14 +0900

    [Gold IV] Title: 알파벳, Time: 1120 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,63 +1,73 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    // GPT: 비트연산자로 변환
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {-1,1,0,0};
+	static boolean[][] visited;
+	static int[][] arr;
+	static int R;
+	static int C;
+	static int result = 0;
+	static boolean alpha[];
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		alpha = new boolean[26];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		arr = new int[R][C];
+		
+		visited = new boolean[R][C];
+		visited[0][0] = true;
+		
+		for(int i=0; i<R; i++) {
+			String line = br.readLine();
+			
+			for(int j=0; j<C; j++) {
+				arr[i][j] = line.charAt(j)-'A';
+			}
+		}
+		alpha[arr[0][0]] = true;
+		dfs(0,0,1);
+		
+		sb.append(result);
+		
+		System.out.println(sb.toString());
+		br.close();
 
-    static int R, C;
-    static char[][] map;
-    static int max = 1;
+	}
+	private static void dfs(int x, int y, int cnt) {
+		
+		result = Math.max(result, cnt);
+		
+		
+		for(int k=0; k<4; k++) {
+			
+			int nx = dx[k] + x;
+			int ny = dy[k] + y;
+			
+			if(nx >= R || ny>= C || nx<0 || ny <0 || visited[nx][ny] || alpha[arr[nx][ny]] ) {
+				continue;
+			}
+			visited[nx][ny] = true;
+			alpha[arr[nx][ny]] = true;
+			dfs(nx, ny, cnt+1);
+			
+			visited[nx][ny] = false;
+			alpha[arr[nx][ny]] = false;
+		}
+		
+	}
 
-    static int[][] mover = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    public static void main(String[] args) throws IOException {
-        int[] input1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        R = input1[0];
-        C = input1[1];
-        map = new char[R][];
-        for (int i = 0; i < R; i++) {
-            map[i] = br.readLine().toCharArray();
-        }
-
-        Point start = new Point(0, 0, 1 << (map[0][0] - 'A'));  // 1을 map[0][0]의 인덱스만큼 좌측으로 이동 (3이였다면 1000 이런 식으로 기록되고, 1인 값은 방문한 값)
-        Stack<Point> stack = new Stack<>();
-        stack.add(start);
-
-        while (!stack.isEmpty()) {
-            Point point = stack.pop();
-            for (int[] move : mover) {
-                int x = move[0] + point.x;
-                int y = move[1] + point.y;
-                if (!isValid(x, y)) continue;
-
-                int newCharBit = 1 << (map[x][y] - 'A'); // 이번에 방문한 인덱스를 만듦
-                if ((point.visited & newCharBit) != 0) continue;    //  양쪽 비트 모두 1인 경우만 남긴 비트가 0이 아니면, 중복된게 있다는 소리
-
-                int newVisited = point.visited | newCharBit;    // 두 비트 중 하나라도 1이면 1 (한번이라도 포함되었으면)
-                stack.add(new Point(x, y, newVisited));
-                int cnt = Integer.bitCount(newVisited); // 비트 수 확인
-                if (cnt > max) max = cnt;
-            }
-        }
-
-        System.out.println(max);
-    }
-
-    static boolean isValid(int x, int y) {
-        return x >= 0 && y >= 0 && x < R && y < C;
-    }
-
-    static class Point {
-        int x, y;
-        int visited;
-
-        Point(int x, int y, int visited) {
-            this.x = x;
-            this.y = y;
-            this.visited = visited;
-        }
-    }
 }
