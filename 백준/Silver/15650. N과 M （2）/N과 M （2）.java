c5a15db2c145714936e74752aa47ff3aea86d7f0
NewChoBo <whwjdrb1009@gmail.com>
2024-08-14 13:15:31 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = input[0];
+        M = input[1];
+
+        // 1부터 N까지 수 중에서 중복 없이 M개를 고른 수열
+        // 고른 수열은 오름차순
+        // ==> 백트래킹
+        for (int i = 1; i <= N; i++) {
+            createArr(new ArrayList<>(), i);
+        }
+        System.out.println(sb);
+    }
+
+    static void createArr(List<Integer> list, int current) {
+        list.add(current);
+        if (list.size() >= M) {
+            for (int num : list) {
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = current + 1; i <= N; i++) {
+            createArr(list, i);
+            list.remove(list.size() - 1);
+        }
+    }
+}
