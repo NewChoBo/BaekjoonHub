4df4de2a011d78ccf4782219ba87c3b951a6e486
NewChoBo <whwjdrb1009@gmail.com>
2024-08-15 08:45:09 +0900

    [Silver II] Title: N과 M (9), Time: 272 ms, Memory: 37312 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] line;
+	static Set<String> stringSet = new HashSet<>();
+
+	public static void main(String[] args) throws IOException {
+		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		N = input[0];
+		M = input[1];
+		line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		Arrays.sort(line);
+
+		// 1부터 N까지 수 중에서 중복 없이 M개를 고른 수열
+		// 고른 수열은 오름차순
+		// ==> 백트래킹
+		for (int i = 0; i < N; i++) {
+			createArr(new ArrayList<>(), i, new boolean[N]);
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+	static void createArr(List<Integer> list, int current, boolean[] visited) {
+		visited[current] = true;
+		list.add(current);
+		if (list.size() >= M) {
+			StringBuilder sb2 = new StringBuilder();
+			for (int num : list) {
+				sb2.append(line[num]).append(' ');
+			}
+			if (!stringSet.contains(sb2.toString())) {
+				stringSet.add(sb2.toString());
+				sb.append(sb2).append('\n');
+			}
+		} else {
+			for (int i = 0; i < N; i++) {
+				if (!visited[i]) {
+					createArr(list, i, visited);
+				}
+			}
+		}
+		visited[current] = false;
+		list.remove(list.size() - 1);
+	}
+}
