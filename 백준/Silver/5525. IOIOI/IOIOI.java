bf76b78d59b88bc8cd5e00349a05ecdd4727469c
NewChoBo <whwjdrb1009@gmail.com>
2024-10-12 19:20:07 +0900

    [Silver I] Title: IOIOI, Time: 232 ms, Memory: 22016 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -12,28 +12,29 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		init();
 		solve();
+		System.out.println(cnt);
 	}
 
 	static void solve() {
-		int start = 0;
-		while (start <= M - (2 * N + 1)) {
-			boolean isPattern = true;
-			for (int i = 0; i < 2 * N + 1; i++) {
-				if ((i % 2 == 0 && S[start + i] != 'I') || (i % 2 == 1 && S[start + i] != 'O')) {
-					isPattern = false;
-					break;
+		int length = 0;
+		for (int i = 2; i < M; i++) {
+			if (S[i] == 'I' && S[i - 1] == 'O' && S[i - 2] == 'I') {
+				length = 3;
+				try {
+					while (S[i + 1] == 'O' && S[i + 2] == 'I') {
+						length += 2;
+						i += 2;
+					}
+				} catch (Exception e) {
 				}
-			}
-
-			if (isPattern) {
-				cnt++;
-				start += 2; // 이후 계속 IOI 패턴이 반복될 수 있으니 바로 다음 검사로 이동
 			} else {
-				start++; // 패턴이 깨졌으면 다음으로 이동
+				length = 0;
+			}
+			int res = ((length - 1) / 2) - N + 1;
+			if (res > 0) {
+				cnt += res;
 			}
 		}
-
-		System.out.println(cnt);
 	}
 
 	static void init() throws IOException {
