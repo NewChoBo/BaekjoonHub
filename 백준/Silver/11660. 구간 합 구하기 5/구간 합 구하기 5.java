3c004d8693a7b1ad0e2ec2f0c2568b7228cf3f5e
NewChoBo <whwjdrb1009@gmail.com>
2024-10-14 12:54:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 932 ms, Memory: 145132 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -12,15 +12,12 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[N][];
-        int[][] sumMap = new int[N][N];
+        int[][] sumMap = new int[N + 1][N + 1];
 
-        for (int i = 0; i < N; i++) {
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int sum = 0;
-            for (int j = 0; j < N; j++) {
-                sum += map[i][j];
-                sumMap[i][j] = sum;
+        for (int i = 1; i < N + 1; i++) {
+            int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 1; j < N + 1; j++) {
+                sumMap[i][j] = array[j - 1] + sumMap[i - 1][j] + sumMap[i][j - 1] - sumMap[i - 1][j - 1];
             }
         }
 
@@ -32,11 +29,7 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int sum = 0;
-            for (int a = x1 - 1; a < x2; a++) {
-                sum += sumMap[a][y2 - 1];
-                if (y1 > 1) sum -= sumMap[a][y1 - 2];
-            }
+            int sum = sumMap[x2][y2] - (sumMap[x1 - 1][y2] + sumMap[x2][y1 - 1]) + sumMap[x1 - 1][y1 - 1];
             sb.append(sum).append('\n');
         }
         System.out.print(sb);
